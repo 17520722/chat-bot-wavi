9914dcca8b1356f0e3eb24652a4fd615d09e6cd4
Nguyen Thanh Luan <50266706+17520722@users.noreply.github.com>
2021-01-11 10:32:53 +0700

    mess link

--- src/components/message-bot/message-bot.component.ts
+++ src/components/message-bot/message-bot.component.ts
@@ -10,14 +10,18 @@ export class MessageBotComponent implements OnInit {
   @Input() content:string = '';
 
   isImg: boolean = false;
+  isLink: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    if (this.content.indexOf('.jpg' || '.png') != -1) {
+    if (this.content.indexOf('.jpg' || '.png') != -1 && 
+        this.content.indexOf('http' || 'https') != -1) {
       this.isImg = true;
     }
-
+    else if (this.content.indexOf('http' || 'https') != -1){
+      this.isLink = true;
+    }
   }
 
 }
